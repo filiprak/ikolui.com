103221c38928595f401c086379dbb5babd533225
Filip Rak <filip.rak@ikol.com>
2025-10-04 17:09:10 +0200

    Update implementation

--- app/components/mdx/CodeBlock.vue
+++ app/components/mdx/CodeBlock.vue
@@ -1,17 +1,13 @@
+<template>
+    <div>
+        <slot></slot>
+    </div>
+</template>
 <script setup lang="ts">
-import { useRender } from '@ikol/ui-kit/composables/render';
-import { h } from 'vue';
 import { CODEBLOCK } from '~/components/mdx/types';
 
-const slots = useSlots();
 const tabs = ref<string[]>([]);
 const active = ref<string>();
 
 provide(CODEBLOCK, { tabs, active });
-
-useRender(() => {
-    return slots.default?.();
-});
-
 </script>
-<style lang="css" module></style>
