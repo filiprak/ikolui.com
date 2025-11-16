b6adb6e31333a96b05af63cf187f85df35c2b464
Filip Rak <filip.rak@ikol.com>
2025-11-16 17:55:44 +0100

    Update implementation

---
+++ app/components/DocsContentNav.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav>
+        <NuxtLink v-for="header in headers"
+                  no-prefetch
+                  :to="`#${header.id}`"
+                  custom
+                  :key="header.uid">
+            <template #default="{ href, isActive }">
+                <a class="ik-py-3 ik-pr-5"
+                   :class="$style.item"
+                   :href="href"
+                   :style="{ paddingLeft: `${(header.level - 1) * 16}px` }">
+                    {{ header.text }}
+                </a>
+            </template>
+        </NuxtLink>
+    </nav>
+</template>
+<script setup lang="ts">
+const { headers } = useHeaders();
+</script>
+<style lang="css" module>
+.item {
+    display: block;
+    text-decoration: none;
+    
+}
+</style>
